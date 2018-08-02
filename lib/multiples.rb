6eabc686c222cd162f2f628786924ab32ffdc89f
github username <github email address>
2018-08-02 09:45:57 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+require "pry"
+def collect_multiples(limit)
+  (1..(limit-1)).select { |x| x % 3 == 0 || x % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(0) {|sum, x| sum + x }
+end
