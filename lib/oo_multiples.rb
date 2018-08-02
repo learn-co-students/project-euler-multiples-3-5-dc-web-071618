f2fff24277eaa0dab4c1359ee8ef2e299f10787f
github username <github email address>
2018-08-02 09:40:53 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...@limit).to_a.select {|n| n%3 == 0 || n%5 == 0}
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each {|a| sum += a}
+    sum
+  end
+
+end
+
+# ret = (1..1000).to_a.select {|n| n%3 == 0 || n%5 == 0}
+#
+# sum = 0
+# ret.each { |a| sum+=a }
+# sum
