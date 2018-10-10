0f94a0313e0dde0bac997d7c19776f707402296b
jensnyder <jlsnyder09@gmail.com>
2018-10-10 09:21:59 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).select{|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  multiples.inject(0){|sum, x| sum + x}
+end
